44397ce59b573e542c35ff25306baff2d8d14167
Ubuntu <ubuntu@ip-172-31-29-11.us-west-2.compute.internal>
2024-05-26 16:56:29 +0000

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
 
   overrides: [
     {
-      files: [],
+      files: ['package.json'],
       plugins: ['eslint-plugin-n8n-nodes-base'],
       extends: ['plugin:n8n-nodes-base/community'],
       rules: {
