b86c14c9db8cb3b15c3d0f02dacdb503ed5bf83f
josh900 <jm5522@sbcglobal.net>
2025-01-09 19:42:10 -0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,6 +53,8 @@ module.exports = {
         'n8n-nodes-base/node-execute-block-missing-continue-on-fail': 'off',
         'n8n-nodes-base/node-resource-description-filename-against-convention': 'off',
         'n8n-nodes-base/node-param-fixed-collection-type-unsorted-items': 'off',
+        'n8n-nodes-base/node-param-description-excess-final-period': 'off',
+        'n8n-nodes-base/node-param-description-missing-final-period': 'off'
       },
     },
   ],
